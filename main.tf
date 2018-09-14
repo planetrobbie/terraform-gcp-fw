8e0085b9662ad79c629137f86771242cddcf2a9b
Sébastien Braun <sebbraun@gmail.com>
2018-09-14 10:28:10 +0200

    variable name reflect provider expectation

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@ resource "google_compute_firewall" "allow-external" {
     protocol = "udp"
   }
 
-  source_ranges = "[${var.external_source_range}]"
+  source_ranges = "[${var.external_source_ranges}]"
 }
 
