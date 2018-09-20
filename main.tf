e2b0135697acbcaa990f9e8022f95a89ef59be61
Sébastien Braun <sebbraun@gmail.com>
2018-09-20 17:30:17 +0200

    vault access on 443 instead of 80

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "google_compute_firewall" "allow-inbound-vault" {
 
   allow {
     protocol = "tcp"
-    ports = ["8200", "80"]
+    ports = ["8200", "443"]
   }
  
   target_tags = ["vault"]
